6fbcf70d53859bc446bd2eec52cab5f2ed7f5992
Ryuji Oda <ryuji.oda@gmail.com>
2021-07-20 23:04:43 +0900

    add: post method

--- src/main/kotlin/example/route/Route.kt
+++ src/main/kotlin/example/route/Route.kt
@@ -1,12 +1,20 @@
 package example.route
 
+import example.request.PostRequest
+import example.response.PostResponse
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Routing.root() {
     get("/") {
         call.respond(HttpStatusCode.OK, "Hello World")
+    }
+
+    post("/") {
+        val request = call.receive<PostRequest>()
+        call.respond(PostResponse("${request.value} posted"))
     }
 }
