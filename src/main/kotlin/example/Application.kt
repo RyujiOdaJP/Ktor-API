6aa965f0e3670490ebd20a7a8fcc5690ad488579
Ryuji Oda <ryuji.oda@gmail.com>
2021-07-20 23:09:56 +0900

    apply to convert json

--- src/main/kotlin/example/Application.kt
+++ src/main/kotlin/example/Application.kt
@@ -2,7 +2,10 @@ package example
 
 import io.ktor.routing.routing
 import example.route.root
-import io.ktor.application.Application
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.jackson.*
+import io.netty.handler.codec.DefaultHeaders
 
 fun main(args: Array<String>) {
     // Nettyを使うための初期設定
@@ -11,6 +14,10 @@ fun main(args: Array<String>) {
 
 @Suppress("unused") // Referenced in application.conf
 fun Application.module() {
+    install(DefaultHeaders)
+    install(ContentNegotiation) {
+        jackson {  }
+    }
     routing {
         root()
     }
